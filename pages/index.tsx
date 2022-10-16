749388cef648db2a0e9c8c7273818407390d6428
nikosantis <nikosantis@gmail.com>
2022-10-16 20:13:15 -0300

    feat: seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import Head from 'next/head'
+
 import WeddingDate from 'components/date'
 import Footer from 'components/footer'
 import Gallery from 'components/gallery'
@@ -7,9 +9,26 @@ import Intro from 'components/intro'
 import OurStory from 'components/our-story'
 import Playlist from 'components/playlist'
 
+const meta = {
+  title: 'Tamy & Mauro ♥ Matrimonio ♥ 18-02-2023',
+  description:
+    'Matrimonio de Tamy & Mauro, 18-02-2023 en Los Aromos 119, Hospital, Paine.',
+
+  type: 'website'
+}
+
 export default function IndexPage() {
   return (
     <div className='w-full relative'>
+      <Head>
+        <title>{meta.title}</title>
+        <meta name='robots' content='follow, index' />
+        <meta content={meta.description} name='description' />
+        <meta property='og:type' content={meta.type} />
+        <meta property='og:site_name' content='Tamy & Mauro' />
+        <meta property='og:description' content={meta.description} />
+        <meta property='og:title' content={meta.title} />
+      </Head>
       <Header />
 
       <Intro />
